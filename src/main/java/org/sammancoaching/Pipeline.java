585843685f05869255b2564ea7b42eee7e7c3bd7
IgorInfnetTPSeASSESSMENTS <beatifuldeath1@gmail.com>
2026-03-16 03:02:46 -0300

    Edit Pipeline - método complexo quebrado em funções menores, add nomes descritivos, add encapsulamento, add enum

--- src/main/java/org/sammancoaching/Pipeline.java
+++ src/main/java/org/sammancoaching/Pipeline.java
@@ -1,9 +1,12 @@
 package org.sammancoaching;
 
 import org.sammancoaching.dependencies.Config;
+import org.sammancoaching.dependencies.DeploymentEnvironment;
 import org.sammancoaching.dependencies.Emailer;
 import org.sammancoaching.dependencies.Logger;
+import org.sammancoaching.dependencies.PipelineStatus;
 import org.sammancoaching.dependencies.Project;
+import org.sammancoaching.dependencies.TestStatus;
 
 public class Pipeline {
     private final Config config;
@@ -17,47 +20,91 @@ public class Pipeline {
     }
 
     public void run(Project project) {
-        boolean testsPassed;
-        boolean deploySuccessful;
+        PipelineResult result = executePipeline(project);
+        sendSummary(result);
+    }
 
-        if (project.hasTests()) {
-            if ("success".equals(project.runTests())) {
-                log.info("Tests passed");
-                testsPassed = true;
-            } else {
-                log.error("Tests failed");
-                testsPassed = false;
-            }
-        } else {
+    private PipelineResult executePipeline(Project project) {
+        if (!testsPassed(project)) {
+            return new PipelineResult(PipelineStatus.TESTS_FAILED);
+        }
+
+        if (!stagingDeploymentSucceeded(project)) {
+            return new PipelineResult(PipelineStatus.STAGING_DEPLOY_FAILED);
+        }
+
+        if (!smokeTestsExist(project)) {
+            log.error("No smoke tests");
+            return new PipelineResult(PipelineStatus.NO_SMOKE_TESTS);
+        }
+
+        if (!smokeTestsPassed(project)) {
+            return new PipelineResult(PipelineStatus.SMOKE_TESTS_FAILED);
+        }
+
+        if (!productionDeploymentSucceeded(project)) {
+            return new PipelineResult(PipelineStatus.PRODUCTION_DEPLOY_FAILED);
+        }
+
+        return new PipelineResult(PipelineStatus.SUCCESS);
+    }
+
+    private boolean testsPassed(Project project) {
+        if (!project.hasTests()) {
             log.info("No tests");
-            testsPassed = true;
+            return true;
         }
 
-        if (testsPassed) {
-            if ("success".equals(project.deploy())) {
-                log.info("Deployment successful");
-                deploySuccessful = true;
-            } else {
-                log.error("Deployment failed");
-                deploySuccessful = false;
-            }
-        } else {
-            deploySuccessful = false;
+        if ("success".equals(project.runTests())) {
+            log.info("Tests passed");
+            return true;
         }
 
-        if (config.sendEmailSummary()) {
-            log.info("Sending email");
-            if (testsPassed) {
-                if (deploySuccessful) {
-                    emailer.send("Deployment completed successfully");
-                } else {
-                    emailer.send("Deployment failed");
-                }
-            } else {
-                emailer.send("Tests failed");
-            }
-        } else {
+        log.error("Tests failed");
+        return false;
+    }
+
+    private boolean stagingDeploymentSucceeded(Project project) {
+        if ("success".equals(project.deploy(DeploymentEnvironment.STAGING))) {
+            log.info("Staging deployment successful");
+            return true;
+        }
+
+        log.error("Staging deployment failed");
+        return false;
+    }
+
+    private boolean smokeTestsExist(Project project) {
+        return project.runSmokeTests() != TestStatus.NO_TESTS;
+    }
+
+    private boolean smokeTestsPassed(Project project) {
+        if (project.runSmokeTests() == TestStatus.PASSING_TESTS) {
+            log.info("Smoke tests passed");
+            return true;
+        }
+
+        log.error("Smoke tests failed");
+        return false;
+    }
+
+    private boolean productionDeploymentSucceeded(Project project) {
+        if ("success".equals(project.deploy(DeploymentEnvironment.PRODUCTION))) {
+            log.info("Production deployment successful");
+            return true;
+        }
+
+        log.error("Production deployment failed");
+        return false;
+    }
+
+    private void sendSummary(PipelineResult result) {
+        if (!config.sendEmailSummary()) {
             log.info("Email disabled");
+            return;
         }
+
+        log.info("Sending email");
+        emailer.send(result.getEmailMessage());
     }
 }
